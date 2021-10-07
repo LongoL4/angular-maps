52685fdd435dd6f5ee30933f331428e23dae7086
LongoL4 <longo.luca@iisgalvanimi.edu.it>
2021-10-07 09:55:06 +0000

    versione 0.1

--- relab/src/app/app.component.ts
+++ relab/src/app/app.component.ts
@@ -13,19 +13,36 @@ export class AppComponent {
   label :string;
   circleOptions: any;
   markerOptions: google.maps.MarkerOptions;
-  
+  vertices: google.maps.LatLngLiteral[]
+
   constructor()
   {
     let iconData : google.maps.Icon = {
       url:'./assets/img/cat_acrobat.ico',
       scaledSize : new google.maps.Size(60,60)
     }
+    
   
+
     this.markerOptions = {icon:iconData}
     this.circleOptions = {fillColor : 'red'}
     this.center={lat: 45.506738, lng: 9.190766};
     this.position = this.center;
     this.label = "ciao";
+    this.vertices = [
+      {  lat: this.center.lat + 0.001, lng: this.center.lng - 0.002 },
+      {  lat: this.center.lat, lng: this.center.lng },
+      {  lat: this.center.lat - 0.001, lng: this.center.lng - 0.002}
+    ];
+
   }
-  
+  yellow(){
+    this.circleOptions = {fillColor : 'yellow'}
+  }
+  red(){
+    this.circleOptions = {fillColor : 'red'}
+  }
+  green(){
+    this.circleOptions = {fillColor : 'green'}
+  }
 }
